8834e21c4a0a7197317fe7ec11919b15237dd93d
Adil <react631@gmail.com>
2023-12-11 18:22:55 +0500

    transaction section in progress

--- src/components/Filter/Transaction.jsx
+++ src/components/Filter/Transaction.jsx
@@ -1,52 +1,60 @@
-import React from 'react'
-import Chip from '@mui/material/Chip';
-import CreditScoreIcon from '@mui/icons-material/CreditScore';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
+import React from "react";
+import Chip from "@mui/material/Chip";
+import CreditScoreIcon from "@mui/icons-material/CreditScore";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-import ClearIcon from '@mui/icons-material/Clear';
+import ClearIcon from "@mui/icons-material/Clear";
+import Box from "@mui/system/Box";
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+
 const Transaction = () => {
+  const [open, setOpen] = React.useState(false);
 
-    const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
-     <Chip onClick={handleClickOpen} sx={{width:'100%',fontSize:'12px'}}  
-     icon={<CreditScoreIcon fontSize='small' />}
-       label="Transaction" /> 
+      <Chip
+        onClick={handleClickOpen}
+        sx={{ width: "100%", fontSize: "12px" }}
+        icon={<CreditScoreIcon fontSize="small" />}
+        label="Transaction"
+      />
 
-{/* <Button variant="outlined" >
+      {/* <Button variant="outlined" >
         Open alert dialog
       </Button> */}
       <Dialog
         open={open}
         fullWidth
-        maxWidth={'lg'}
+        maxWidth={"lg"}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
-
       >
-        <DialogTitle id="alert-dialog-title"
-         sx={{
-            display:'flex',
-         justifyContent:"space-between",
-         alignItems:'center'
-         }}>
-        <Typography
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography
             variant="h2"
             sx={{
               fontWeight: "400",
@@ -60,36 +68,102 @@ const Transaction = () => {
           >
             Create
           </Typography>
-          <ClearIcon 
-          fontSize='small' 
-          onClick={handleClose}
-          sx={{
-            color:'orange', 
-            padding:'5px',
-            fontSize:"30px",
-            borderRadius:"20px",
-            cursor:'pointer',
-            '&:hover': {
-                backgroundColor: 'orange', 
-                color:"white"
+          <ClearIcon
+            fontSize="small"
+            onClick={handleClose}
+            sx={{
+              color: "orange",
+              padding: "5px",
+              fontSize: "30px",
+              borderRadius: "20px",
+              cursor: "pointer",
+              "&:hover": {
+                backgroundColor: "orange",
+                color: "white",
               },
-            }} />
+            }}
+          />
         </DialogTitle>
         <Divider />
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            <br/>
-            location data to Google, even when no apps are running.
+            <Box>
+              <Button
+                sx={{
+                  bgcolor: "#FF9F43",
+                  color: "white",
+                  textTransform: "capitalize",
+                  fontFamily: "Titillium Web, sans-serif",
+                  fontSize: "16px",
+                  padding: "10px 15px",
+                  letterSpacing: "1px",
+                  marginRight: "20px",
+                  "&:hover": {
+                    backgroundColor: "#FF9F43",
+                  },
+                }}
+              >
+                Purchase
+              </Button>
+
+              <Button
+                sx={{
+                  bgcolor: "#FF9F43",
+                  color: "white",
+                  textTransform: "capitalize",
+                  fontFamily: "Titillium Web, sans-serif",
+                  fontSize: "16px",
+                  padding: "10px 15px",
+                  letterSpacing: "1px",
+                  marginRight: "20px",
+                  "&:hover": {
+                    backgroundColor: "#FF9F43",
+                  },
+                }}
+              >
+                Payment
+              </Button>
+
+              <Button
+                sx={{
+                  bgcolor: "#FF9F43",
+                  color: "white",
+                  textTransform: "capitalize",
+                  fontFamily: "Titillium Web, sans-serif",
+                  fontSize: "16px",
+                  padding: "10px 15px",
+                  letterSpacing: "1px",
+                  marginRight: "20px",
+                  "&:hover": {
+                    backgroundColor: "#FF9F43",
+                  },
+                }}
+              >
+                return
+              </Button>
+            </Box>
+            {/* search bar and operation  */}
+            <Box sx={{margin:"40px 0"}}>
+            <TextField
+          label="Search"
+          size="small"
+          id="outlined-start-adornment"
+          
+          sx={{}}
+          InputProps={{
+            startAdornment: 
+            <InputAdornment position="start">
+              <SearchIcon/>
+                </InputAdornment>,
+          }}
+        />
+            </Box>
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          
-        </DialogActions>
+        <DialogActions></DialogActions>
       </Dialog>
-
     </>
-  )
-}
+  );
+};
 
-export default Transaction
+export default Transaction;
