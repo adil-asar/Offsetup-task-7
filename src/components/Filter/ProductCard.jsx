e682f0d0b44dee3deb1b0c3947152e59a320cbe5
Adil <react631@gmail.com>
2023-12-14 15:13:03 +0500

    hover effect

--- src/components/Filter/ProductCard.jsx
+++ src/components/Filter/ProductCard.jsx
@@ -19,13 +19,16 @@ const ProductCard = ({name,source,categ,price,quantity,itemid}) => {
   };
   return (
     <div    className={`product_card ${isActive ? 'product_added' : ''}`}  onClick={handleAddToCart}>
-{isActive && <CheckBoxIcon className='icon_check' fontSize='large' />}
-      <div className="image_container">
-      <img src={source} alt="" />
-      </div>
-      <p style={{fontSize:"15px",marginBottom:'5px',letterSpacing:'1px'}}>{categ}</p>
-      <strong style={{letterSpacing:'2px'}}>{name}</strong>
-      <p style={{color:'red',marginBottom:'15px',marginTop:'20px'}}>{price} $</p>
+
+<div className='image_container'>
+<img src={source} alt="" />
+</div>
+{isActive && <CheckBoxIcon className='icon_css' fontSize='small' />}
+
+      <p style={{fontSize:"15px",marginTop:'15px',letterSpacing:'1px',marginBottom:'10px',}}>{categ}</p>
+      <strong style={{letterSpacing:'2px',}}>{name}</strong>
+      <p style={{color:'red',marginBottom:'15px',marginTop:'5px'}}>{price} $</p>
+
     </div>
   )
 }
